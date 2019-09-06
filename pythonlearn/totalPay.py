aa12c732fb0a2f7841d96ba6f4b5928eaa399b8e
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2019-09-05 22:11:50 -0700

    Create totalPay.py

---
+++ pythonlearn/totalPay.py
@@ -0,0 +1,6 @@
+# Write a program to prompt the user for hours and rate per
+# hour to compute gross pay.
+
+userHours = input('Enter your hours worked: ')
+userPay = input('Enter the rate per hour: ')
+print('Your total pay today is: ', int(userHours) * int(userPay))
