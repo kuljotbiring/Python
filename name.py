c4dd2ccb0d7fff5b08ab476137c70b1a0acaf559
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2019-11-10 22:42:33 -0800

    Update name.py

--- name.py
+++ name.py
@@ -1,4 +1,9 @@
 # print the string with each word capitalized
+# print the string with all words capitalized
+# print the string with all words lowercase
 
 name = "ada lovelace"
 print(name.title())
+print(name.upper())
+print(name.lower())
+
