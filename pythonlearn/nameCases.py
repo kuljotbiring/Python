8d646e3a5c9542085d9d853b74a122242906303a
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-03-21 23:54:25 -0700

    Create nameCases.py

---
+++ pythonlearn/nameCases.py
@@ -0,0 +1,5 @@
+# Use a variable to represent a person’s name, and then print
+# that person’s name in lowercase, uppercase, and title case.
+
+name = "kuljot biring"
+print(f"{name.lower()}\n{name.upper()}\n{name.title()}")
