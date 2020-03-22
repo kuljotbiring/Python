7bf3977c6f8070b946794a07468754b7a61af3d5
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-03-22 00:06:40 -0700

    Create number_eight.py

---
+++ pythonlearn/number_eight.py
@@ -0,0 +1,8 @@
+# Write addition, subtraction, multiplication, and division
+# operations that each result in the number 8. Be sure to enclose your operations
+# in print() calls to see the results.
+
+print(5+3)
+print(20-12)
+print(4*2)
+print(72/9)
