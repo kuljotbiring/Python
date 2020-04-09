ebe8881922d089c03ae869196e533f5c14f97b4d
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-04-08 20:37:52 -0700

    Create file_work.py

---
+++ pythonlearn/file_work.py
@@ -0,0 +1,27 @@
+# Open a blank file in your text editor and write a few
+# lines summarizing what you’ve learned about Python so far. Start each line
+# with the phrase In Python you can. . . . Save the file as learning_python.txt in
+# the same directory as your exercises from this chapter. Write a program that
+# reads the file and prints what you wrote three times.
+
+
+# Print the contents once by
+# reading in the entire file
+with open('learning_python.txt') as file_object:
+    data = file_object.read()
+
+print(data)
+
+# once by looping over the file object
+with open('learning_python.txt') as file_object:
+    for line in file_object:
+        print(line.strip())
+
+# and once by storing
+# the lines in a list and then working with them outside the with block.
+
+with open('learning_python.txt') as file_object:
+    my_list = file_object.readlines()
+
+for file_line in my_list:
+    print(file_line.strip())
