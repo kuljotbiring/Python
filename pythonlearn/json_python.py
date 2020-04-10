926ee63a8f1467b27ecd4f7c2111ace02df887b3
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-04-09 21:58:21 -0700

    Create json_python.py

---
+++ pythonlearn/json_python.py
@@ -0,0 +1,40 @@
+import json
+
+
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        print(f"ERROR: Could not open the file {filename}")
+    else:
+        return username
+
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        correct = input("Are you " + username + "? (y/n) ")
+        if correct == 'y':
+            print("Welcome back, " + username + "!")
+        else:
+            username = get_new_username()
+            print("We'll remember you when you come back, " + username + "!")
+    else:
+        username = get_new_username()
+        print("We'll remember you when you come back, " + username + "!")
+
+
+greet_user()
