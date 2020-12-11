6d41ac4867615ec19e6e1bab8431f89f5d945770
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-12-10 22:56:01 -0800

    Update animals.py

--- pythonlearn/animals.py
+++ pythonlearn/animals.py
@@ -1,4 +1,4 @@
-# Think of at least three different animals that have a common characteristic.
+# Think of at least three different animals that have some common characteristic.
 # Store the names of these animals in a list, and then use a for loop to
 # print out the name of each animal.
 favorite_animals = ['lion', 'tiger', 'jaguar']
